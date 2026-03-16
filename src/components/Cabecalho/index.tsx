2f4d2d599339cc6719bb8111b9c2cd683d967758
ElMathidones <seuemail@exemplo.commath.mendez.007@gmail.com>
2026-03-16 19:37:37 -0300

    Exercicio Styled Components concluido

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,20 @@
-import styles from './Cabecalho.module.css'
+import styled from 'styled-components'
+
+const Header = styled.header`
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  text-align: center;
+  padding: 24px 0;
+`
+
+const Titulo = styled.h1`
+  font-size: 32px;
+`
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
-    <h1>EBAC Jobs</h1>
-  </header>
+  <Header>
+    <Titulo>EBAC Jobs</Titulo>
+  </Header>
 )
 
 export default Cabecalho
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,56 @@
-import styles from './Hero.module.css'
+import styled from 'styled-components'
+
+const HeroSection = styled.form`
+  height: 360px;
+  width: 100%;
+  background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  &::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--cor-principal);
+    content: '';
+    opacity: 0.7;
+  }
+
+  @media (max-width: 768px) {
+    height: auto;
+    padding: 24px 0;
+  }
+`
+
+const Conteudo = styled.div`
+  position: relative;
+  color: #eee;
+`
+
+const Titulo = styled.h2`
+  font-family: Gloock, serif;
+  font-size: 48px;
+
+  @media (max-width: 768px) {
+    font-size: 32px;
+  }
+`
 
 const Hero = () => (
-  <form className={styles.form}>
+  <HeroSection>
     <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
+      <Conteudo>
+        <Titulo>
+          As melhores vagas para tecnologia, design e artes visuais.
+        </Titulo>
+      </Conteudo>
     </div>
-  </form>
+  </HeroSection>
 )
 
 export default Hero
